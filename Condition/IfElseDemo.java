8637334f6ed15a5b867994bf846f3fb305a292fd
themdshakibul <mdshakibul88240@gmail.com>
2026-07-03 09:16:29 +0600

    add: the if else condtion clear

--- Condition/IfElseDemo.java
+++ Condition/IfElseDemo.java
@@ -1,6 +1,12 @@
 public class IfElseDemo {
-    public static void main(String[] args) {
+    /*
+        Simple if else statement
+        if-else statement
+        if-else-if ladder
+        Nested if-statement
+    */
 
+    public static void main(String[] args) {
         //  initialize a variable with int value. if the variable is even the print even.
 
         int num = 10;
@@ -8,6 +14,22 @@ public class IfElseDemo {
             System.out.println("Even Number");
         } else {
             System.out.println("Odd Number");
+        }
+
+        /*
+            mark, mark >= 90 A+
+            mark <90 and mark > 80 > a
+            mark < 80 and mark > 70 B
+        */
+        int mark = 90;
+        if(mark >= 90){
+            System.out.println("A+");
+        }else if(mark < 90 && mark > 80){
+            System.out.println("A");
+        }else if(mark < 80 && mark > 70){
+            System.out.println("B");
+        }else{
+            System.out.println("C");
         }
     }
 }
